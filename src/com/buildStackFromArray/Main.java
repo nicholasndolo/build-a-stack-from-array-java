9532a43c6441de68cdf28fa1ffd12435bc483251
Nicholas <nickndolo95@gmail.com>
2024-10-01 08:59:15 +0300

    add pop method

--- src/com/buildStackFromArray/Main.java
+++ src/com/buildStackFromArray/Main.java
@@ -2,5 +2,12 @@ package com.buildStackFromArray;
 
 public class Main {
     public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.pop();
+
+        System.out.println(stack);
     }
 }
